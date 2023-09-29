d2336a2de512c4f14088e9435d7c3d083dfa6bb2
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-09-29 15:04:16 -0700

    Add element variables

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var startButton = document.getElementById("start-button");
+var resetScoreButton = document.getElementById("reset-score-button");
+
+var mysteryWordEl = document.getElementById("mystery-word");
+
+var winCountEl = document.getElementById("winCount");
+var lossCountEl = document.getElementById("lossCount");
+
+var countdownEl = document.getElementById("countdown");
+
+
+
+// eventListener for START button
+// eventListener for RESET button
+// need Array of mystery words
+// need interval timer to get countdown
+
+
